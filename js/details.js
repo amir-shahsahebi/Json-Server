9b432b29cf94e0dd384879c70d9df0a42935c060
amir shahsahebi <shahsahebiamir@gmail.com>
2022-03-22 18:49:51 +0430

    URLSearchParms added to grab id and create html..

--- js/details.js
+++ js/details.js
@@ -1,1 +1,16 @@
 // javascript for details.html
+const id = new URLSearchParams(window.location.search).get('id')  //window.lacation.search equal to: ?id=2  AND "id" equal to key
+
+const container = document.querySelector(".details") 
+
+const renderPosts = async () => {
+    const res = await fetch("http://localhost:3000/posts/"+id);
+    const post = await res.json()
+    console.log(post)
+    const template = `
+    <h1>${post.title}</h1>
+    <p>${post.body}</p>
+    `
+    container.innerHTML= template
+}
+window.addEventListener("DOMContentLoaded", () => renderPosts());
