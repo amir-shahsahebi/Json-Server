a7bda9912ec4056f243c22e56b47411c603a0233
amir shahsahebi <shahsahebiamir@gmail.com>
2022-03-22 21:03:49 +0430

    deleting post added in details.js

--- js/details.js
+++ js/details.js
@@ -2,7 +2,7 @@
 const id = new URLSearchParams(window.location.search).get('id')  //window.lacation.search equal to: ?id=2  AND "id" equal to key
 
 const container = document.querySelector(".details") 
-
+const deleteBtn = document.querySelector(".delete")
 const renderPosts = async () => {
     const res = await fetch("http://localhost:3000/posts/"+id);
     const post = await res.json()
@@ -13,4 +13,12 @@ const renderPosts = async () => {
     `
     container.innerHTML= template
 }
+
+deleteBtn.addEventListener("click",async (e)=> {
+    const res = await fetch("http://localhost:3000/posts/"+id, {
+        method: "DELETE"
+    });
+
+    window.location.replace("\index.html")
+})
 window.addEventListener("DOMContentLoaded", () => renderPosts());
